c6b03ca26881597750766424c0cefc3e5fe0026f
crumpl07 <crumpst3r2000@gmail.com>
2021-07-07 10:38:44 -0500

    Added some autonomy for scenic-sumo

--- scenic-sumo.py
+++ scenic-sumo.py
@@ -2,7 +2,7 @@ import os, sys
 import scenic
 import traci
 
-scenario = scenic.scenarioFromFile("C:\\Users\crump\\OneDrive\\Documents\\GitHub\\Scenic-Sumo\\behindCarWithPedestrian.scenic")
+scenario = scenic.scenarioFromFile("C:\\Users\\crump\\OneDrive\\Documents\\GitHub\\Scenic-Sumo\\behindCarWithPedestrian.scenic")
 
 scene = scenario.generate(maxIterations = 1, verbosity = 0, feedback = None)
 sumoBinary = 'sumo-gui'
@@ -12,21 +12,29 @@ traci.start(sumoCmd)
 count = 0
 trip = "trip" + str(count)
 
-#Creates all the routes
 for x in scene[0].objects:
-    trip = "trip" + str(count)
-    traci.route.add(trip, x.route)
-    count += 1
 
-count = 0
-#Creates all vehicles and places them on routes
-for x in scene[0].objects:
-    trip = "trip" + str(count)
-    traci.vehicle.add(x.name, trip)
-    count += 1
+    if str(type(x)) == "<class 'scenic.simulators.sumo.model.Car'>":
 
-#sets the top actor x units ahead
-traci.vehicle.moveTo(scene[0].objects[1].name, 'e1_0', scene[0].objects[1].distance)
+        #Create routes
+        trip = "trip" + str(count)
+        traci.route.add(trip, x.route)
+        
+        #Create vehicles
+        traci.vehicle.add(x.name, trip)
+        
+        #Set Distance 
+        if x.distance != 0:
+            road = str(x.route)
+            road = road.split("'")
+            traci.vehicle.moveTo(x.name, road[1] + '_' + str(x.lane), x.distance)
+        count += 1    
+
+    if str(type(x)) == "<class 'scenic.simulators.sumo.model.Pedestrian'>":
+        print("PEDESTRIAN NOT CURRENTLY SUPPORTED!")
+
+    if str(type(x)) == "<class 'scenic.simulators.sumo.model.Biker>":
+        print("BIKER NOT CURRENTLY SUPPORTED!")
 
 #Runs simulation to completion
 while traci.simulation.getMinExpectedNumber() > 0:
