fc8e2a15fb0a6829ec97d488895061b207053d25
Kai Sniadach <107019201+KaiSniadach@users.noreply.github.com>
2022-07-12 18:12:21 -0400

    7/12/22 commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""World models and interfaces for particular simulators.
+
+.. raw:: html
+
+   <h2>Submodules</h2>
+
+.. autosummary::
+   :toctree:
+
+   newtonian
+   carla
+   gta
+   lgsvl
+   webots
+   xplane
+   utils
+"""
